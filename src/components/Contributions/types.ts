ab29e27eacc9002e1509e241b70765c9b6b0db05
Adrian Stroila <adrian.stroila@vivre.eu>
2025-06-07 05:50:25 +0300

    chore: update types and Netlify configuration

--- src/components/Contributions/types.ts
+++ src/components/Contributions/types.ts
@@ -1,13 +1,13 @@
-export interface Contributor {
-  author: string
-  username: string
-  value: number
-}
-
 export interface User {
   author: string
   username: string
   coordinates: [number, number]
+}
+
+export interface Contributor {
+  author: string
+  username: string
+  value: number
 }
 
 export enum ELeaderKeys {

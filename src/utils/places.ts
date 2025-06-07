0a334206c29cd15d5cc2df709f0fc1135beee656
Adrian Stroila <adrian.stroila@vivre.eu>
2025-06-07 05:33:40 +0300

    feat: add places data file

---
+++ src/utils/places.ts
@@ -0,0 +1,42 @@
+export const PLACES = [
+  {
+    name: 'Eiffel Tower',
+    lat: 48.8584,
+    lng: 2.2945,
+    leader1: 10,
+    leader2: 5,
+    leader3: 8
+  },
+  {
+    name: 'Statue of Liberty',
+    lat: 40.6892,
+    lng: -74.0445,
+    leader1: 8,
+    leader2: 12,
+    leader3: 6
+  },
+  {
+    name: 'Taj Mahal',
+    lat: 27.1751,
+    lng: 78.0421,
+    leader1: 15,
+    leader2: 7,
+    leader3: 9
+  },
+  {
+    name: 'Great Wall of China',
+    lat: 40.4319,
+    lng: 116.5704,
+    leader1: 12,
+    leader2: 9,
+    leader3: 11
+  },
+  {
+    name: 'Machu Picchu',
+    lat: -13.1631,
+    lng: -72.5450,
+    leader1: 9,
+    leader2: 14,
+    leader3: 7
+  }
+] 
